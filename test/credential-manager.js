08a9df4eaa01bd9babbac51775de35a2e2324d80
Chie Ichongiri <Chiemezie.Ichongiri@wlt.com>
2020-11-28 14:11:06 +0000

    Initialising unit tests for CredentialManager. Updated eslint config

---
+++ test/credential-manager.js
@@ -0,0 +1,40 @@
+const chai = require('chai')
+const dirtyChai = require('dirty-chai')
+const expect = chai.expect
+const sinon = require('sinon')
+const inquirer = require('inquirer')
+const CredentialManager = require('../lib/credential-manager')
+
+chai.use(dirtyChai)
+
+describe('a credential manager', () => {
+  let creds
+
+  before(() => {
+    creds = new CredentialManager('pof-test')
+  })
+
+  context('with no existing credentials', () => {
+    it('should prompt the user', async () => {
+      sinon.stub(inquirer, 'prompt').resolves({ key: 'foo', secret: 'bar' })
+      const [key, secret] = await creds.getKeyAndSecret()
+      expect(key).to.equal('foo')
+      expect(secret).to.equal('bar')
+      expect(inquirer.prompt.calledOnce).to.be.true()
+      inquirer.prompt.restore()
+    })
+  })
+
+  context('with existing credentials', () => {
+    it('should just return them', async () => {
+      const [key, secret] = await creds.getKeyAndSecret()
+      expect(key).to.equal('foo')
+      expect(secret).to.equal('bar')
+    })
+  })
+
+  after(() => {
+    creds.conf.delete('apiKey')
+    creds.conf.delete('apiSecret')
+  })
+})
