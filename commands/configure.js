79fae4ad6a1dce9a8a005a487b87b10097f59f9f
Chie Ichongiri <Chiemezie.Ichongiri@wlt.com>
2020-11-29 06:46:36 +0000

    Introducing try...catch block for improved error-handling

--- commands/configure.js
+++ commands/configure.js
@@ -6,56 +6,63 @@ const App = require('../lib/app')
 
 const configure = {
   async consumer (name) {
-    const creds = new CredentialManager(name)
-    const answers = await inquirer.prompt([
-      {
-        type: 'input',
-        name: 'key',
-        message: 'Enter your API Key:',
-        validate: util.notEmpty
-      },
-      {
-        type: 'password',
-        name: 'secret',
-        message: 'Enter your API Secret:',
-        validate: util.notEmpty
-      }
-    ])
-    await creds.storeKeyAndSecret('apiKey', answers.key, answers.secret)
+    try {
+      const creds = new CredentialManager(name)
+      const answers = await inquirer.prompt([
+        {
+          type: 'input',
+          name: 'key',
+          message: 'Enter your API Key:',
+          validate: util.notEmpty
+        },
+        {
+          type: 'password',
+          name: 'secret',
+          message: 'Enter your API Secret:',
+          validate: util.notEmpty
+        }
+      ])
+      await creds.storeKeyAndSecret('apiKey', answers.key, answers.secret)
+    } catch (error) {
+      console.log(error.message)
+    }
   },
 
   async account (name) {
-    const creds = new CredentialManager(name)
-    const [apiKey, apiSecret] = await creds.getKeyAndSecret('apiKey')
-    const app = new App(apiKey, apiSecret)
-    const response = querystring.parse(await app.post('oauth/request_token'))
-    app.setToken(response.oauth_token, response.oauth_token_secret)
-    await inquirer.prompt({
-      type: 'input',
-      message: 'Press Enter to open Twitter in your default browser to authorise access',
-      name: 'continue'
-    })
+    try {
+      const creds = new CredentialManager(name)
+      const [apiKey, apiSecret] = await creds.getKeyAndSecret('apiKey')
+      const app = new App(apiKey, apiSecret)
+      const response = querystring.parse(await app.post('oauth/request_token'))
+      app.setToken(response.oauth_token, response.oauth_token_secret)
+      await inquirer.prompt({
+        type: 'input',
+        message: 'Press Enter to open Twitter in your default browser to authorise access',
+        name: 'continue'
+      })
 
-    util.openBrowser(`${app.baseURL}oauth/authorize?oauth_token=${response.oauth_token}`)
+      util.openBrowser(`${app.baseURL}oauth/authorize?oauth_token=${response.oauth_token}`)
 
-    const answers = inquirer.prompt({
-      type: 'input',
-      message: 'Enter the PIN provided by App',
-      name: 'pin',
-      validate: util.notEmpty
-    })
+      const answers = inquirer.prompt({
+        type: 'input',
+        message: 'Enter the PIN provided by App',
+        name: 'pin',
+        validate: util.notEmpty
+      })
 
-    const tokenResponse = querystring.parse(
-      await app.post('oauth/access_token', `oauth_verifier=${answers.pin}`)
-    )
+      const tokenResponse = querystring.parse(
+        await app.post('oauth/access_token', `oauth_verifier=${answers.pin}`)
+      )
 
-    app.setToken(tokenResponse.oauth_token, tokenResponse.oauth_token_secret)
+      app.setToken(tokenResponse.oauth_token, tokenResponse.oauth_token_secret)
 
-    const verifyResponse = await app.get('1.1/account/verify_credentials.json')
-    await creds.storeKeyAndSecret('accountToken', tokenResponse.oauth_token, tokenResponse.oauth_token_secret
-    )
+      const verifyResponse = await app.get('1.1/account/verify_credentials.json')
+      await creds.storeKeyAndSecret('accountToken', tokenResponse.oauth_token, tokenResponse.oauth_token_secret)
 
-    console.log(`Account "${verifyResponse.screen_name}" successfully added`)
+      console.log(`Account "${verifyResponse.screen_name}" successfully added`)
+    } catch (error) {
+      console.log(error.message)
+    }
   }
 }
 
